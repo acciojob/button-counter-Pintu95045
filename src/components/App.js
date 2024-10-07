573aeab7d02f08ec24c6e1550d3d8e36cdb022a9
Pintu Giri <mrpintugiri@gmail.com>
2024-10-07 14:42:33 +0530

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
